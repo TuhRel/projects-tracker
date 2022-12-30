6eaa5c57a764f56cf887064b5e1db3916bb4656e
TuhRel <tonytj0106@gmail.com>
2022-12-30 13:34:30 -0500

    removed 'Project' file and project imports

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const   express       = require('express');
 const   cors          = require('cors');
 const   dbOperation   = require('./dbFiles/dbOperation');
-const   Project       = require('./dbFiles/project');
 
 const API_PORT = process.env.PORT || 5000;
 const app = express();
@@ -21,10 +20,5 @@ app.post('/hello', async(req, res) => {
   const result = await dbOperation.getProjects(req.body.ProjectName);
   res.send(result.recordset);
 })
-
-let BlackJack = new Project(103, 'BlackJack App', 'Blackjack app created for iOS and Android, that also teaches basic blackjack strategy.', 'February 1, 2023', 'May 1, 2023', 'Swift, JavaScript, Adroid Development, Mobile Development, Construct');
-
-// console.log(ListPull)
-
 
 app.listen(API_PORT, () => console.log(`listening on port ${API_PORT}`));
